497b510e7b8ccce0c83ef4eb9d9ff6f5cfb1f5a3
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:03:23 +0700

    Upload app/Http/Controllers/Auth/LoginController.php

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Enums\RoleEnum;
+
+class LoginController extends Controller
+{
+    public function showLoginForm($role)
+    {
+        return view('auth.login', compact('role'));
+    }
+
+    public function login(Request $request, $role)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            if ($user->role->value === $role) {
+                $request->session()->regenerate();
+                return redirect()->intended(route($role . '.dashboard'));
+            } else {
+                Auth::logout();
+                return back()->withErrors([
+                    'email' => 'Anda tidak memiliki akses ke peran ini.',
+                ])->onlyInput('email');
+            }
+        }
+
+        return back()->withErrors([
+            'email' => 'Kredensial yang diberikan tidak cocok dengan catatan kami.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
