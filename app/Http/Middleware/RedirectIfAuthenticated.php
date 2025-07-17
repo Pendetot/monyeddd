464de5465576cf9e777a8c5a1a71b01a5e27b351
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:09 +0700

    Upload app/Http/Middleware/RedirectIfAuthenticated.php

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): Response
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                if ($guard === 'superadmin') {
+                    return redirect()->route('superadmin.dashboard');
+                }
+                if ($guard === 'hrd') {
+                    return redirect()->route('hrd.dashboard');
+                }
+                if ($guard === 'keuangan') {
+                    return redirect()->route('keuangan.dashboard');
+                }
+                if ($guard === 'operasional') {
+                    return redirect()->route('operasional.dashboard');
+                }
+                return redirect(RouteServiceProvider::HOME);
+            }
+        }
+
+        return $next($request);
+    }
+}
