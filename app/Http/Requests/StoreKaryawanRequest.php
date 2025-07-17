960ed16ce7172daae4bd769072efa10c6860faed
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:22 +0700

    Upload app/Http/Requests/StoreKaryawanRequest.php

---
+++ app/Http/Requests/StoreKaryawanRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreKaryawanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama' => 'required|string|max:255',
+            'nik' => 'required|string|max:255|unique:karyawans',
+            'alamat' => 'required|string',
+            'telepon' => 'required|string|max:20',
+            'jabatan' => 'required|string|max:255',
+            'penempatan' => 'required|string|max:255',
+            'status' => ['required', Rule::in(['aktif', 'nonaktif'])],
+        ];
+    }
+}
