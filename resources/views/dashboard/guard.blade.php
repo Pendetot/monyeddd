9ecd3e78663a3d25998338d1213f21e1966ca1fc
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:27:02 +0700

    Upload resources/views/dashboard/guard.blade.php

---
+++ resources/views/dashboard/guard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">Dashboard Operasional</div>
+            <div class="card-body">
+                Selamat datang, Operasional! Ini adalah dashboard khusus untuk Anda.
+                <!-- Tambahkan konten spesifik Operasional di sini -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
