d8de4947275c397c0ee9cf81e8330b47b91d9f9c
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:29:19 +0700

    Upload resources/views/superadmin/dashboard.blade.php

---
+++ resources/views/superadmin/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Super Admin Dashboard')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Selamat Datang di Dashboard Super Admin</h3>
+                </div>
+                <div class="card-body">
+                    <p>Ini adalah dashboard khusus untuk peran Super Admin.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
