cfdb1f1187ad2a6e1f8ec0915a6d528580114406
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:28:36 +0700

    Upload resources/views/layouts/partials/sidebar-guard.blade.php

---
+++ resources/views/layouts/partials/sidebar-guard.blade.php
@@ -0,0 +1,16 @@
+<ul class="pc-navbar">
+    <li class="pc-item pc-caption">
+        <label>Navigation</label>
+    </li>
+    <li class="pc-item">
+        <a href="{{ route('guard.dashboard') }}" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-dashboard"></i></span>
+            <span class="pc-mtext">Dashboard</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>Manajemen Guard</label>
+    </li>
+    {{-- Add Guard specific menu items here --}}
+</ul>
