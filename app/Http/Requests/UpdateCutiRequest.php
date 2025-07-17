4136b34e28f07e6aefdee4ed262e028136f976e9
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:29 +0700

    Upload app/Http/Requests/UpdateCutiRequest.php

---
+++ app/Http/Requests/UpdateCutiRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateCutiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'tanggal_mulai' => 'required|date',
+            'tanggal_selesai' => 'required|date|after_or_equal:tanggal_mulai',
+            'jenis_cuti' => ['required', Rule::in(['tahunan', 'sakit', 'melahirkan', 'penting'])],
+            'status' => ['required', Rule::in(['pending', 'disetujui', 'ditolak'])],
+            'alasan' => 'required|string',
+        ];
+    }
+}
