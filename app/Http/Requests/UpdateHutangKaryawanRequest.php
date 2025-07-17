93513e6dbfbec943d1c08d9f60b9c0e0f0a5bb3b
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:31 +0700

    Upload app/Http/Requests/UpdateHutangKaryawanRequest.php

---
+++ app/Http/Requests/UpdateHutangKaryawanRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateHutangKaryawanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'jumlah' => 'required|numeric|min:0',
+            'alasan' => 'required|string',
+            'status' => ['required', Rule::in(['lunas', 'belum_lunas'])],
+            'asal_hutang' => ['required', Rule::in(['sp', 'pinjaman'])],
+        ];
+    }
+}
