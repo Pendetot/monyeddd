6f395642bb82060642342d8389b7533b6dd45993
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:42 +0700

    Upload app/Http/Requests/HRD/Cuti/StoreCutiRequest.php

---
+++ app/Http/Requests/HRD/Cuti/StoreCutiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\HRD\Cuti;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCutiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'tanggal_mulai' => 'required|date',
+            'tanggal_selesai' => 'required|date|after_or_equal:tanggal_mulai',
+            'jenis_cuti' => 'required|string|max:255',
+            'status' => 'required|in:pending,approved,rejected',
+        ];
+    }
+}
