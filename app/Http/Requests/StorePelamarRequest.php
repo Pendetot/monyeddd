f37bf81a380bbcaedcfca8d47432b26777d6a355
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:25 +0700

    Upload app/Http/Requests/StorePelamarRequest.php

---
+++ app/Http/Requests/StorePelamarRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePelamarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:pelamars',
+            'telepon' => 'required|string|max:20',
+            'alamat' => 'required|string',
+            'status_aplikasi' => 'nullable|string',
+            'tanggal_interview' => 'nullable|date',
+            'catatan_hrd' => 'nullable|string',
+        ];
+    }
+}
