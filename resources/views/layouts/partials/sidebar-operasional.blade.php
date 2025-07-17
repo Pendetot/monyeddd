628e4268ff0e1ec28a6e6826b89ef18268ced372
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:28:42 +0700

    Upload resources/views/layouts/partials/sidebar-operasional.blade.php

---
+++ resources/views/layouts/partials/sidebar-operasional.blade.php
@@ -0,0 +1,21 @@
+<ul class="pc-navbar">
+    <li class="pc-item pc-caption">
+        <label>Navigation</label>
+    </li>
+    <li class="pc-item">
+        <a href="{{ route('operasional.dashboard') }}" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-dashboard"></i></span>
+            <span class="pc-mtext">Dashboard</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>Manajemen Operasional</label>
+    </li>
+    <li class="pc-item">
+        <a href="{{ route('kpis.index') }}" class="pc-link">
+            <span class="pc-micon"><i class="ti ti-chart-line"></i></span>
+            <span class="pc-mtext">Penilaian KPI</span>
+        </a>
+    </li>
+</ul>
