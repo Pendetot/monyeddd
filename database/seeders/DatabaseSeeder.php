24012f7c19fa5c2ba898fe9c06df0be576bb8fc6
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:06:32 +0700

    Upload database/seeders/DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(UserSuperAdminSeeder::class);
+        $this->call(PelamarSeeder::class);
+        $this->call(KaryawanSeeder::class);
+        $this->call(CutiSeeder::class);
+        $this->call(ResignSeeder::class);
+        $this->call(SuratPeringatanSeeder::class);
+    }
+}
