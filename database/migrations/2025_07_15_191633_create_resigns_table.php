92014562e4f666b384745add256404c87c3054e6
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:06:04 +0700

    Upload database/migrations/2025_07_15_191633_create_resigns_table.php

---
+++ database/migrations/2025_07_15_191633_create_resigns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('karyawan_id')->constrained('karyawans')->onDelete('cascade');
+            $table->date('tanggal_pengajuan');
+            $table->date('tanggal_efektif');
+            $table->text('alasan');
+            $table->enum('status', ['pending', 'disetujui', 'ditolak'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resigns');
+    }
+};
