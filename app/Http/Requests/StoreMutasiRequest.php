68094fa4aa16b55cb3fae2fe6253c0815f077d21
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:24 +0700

    Upload app/Http/Requests/StoreMutasiRequest.php

---
+++ app/Http/Requests/StoreMutasiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMutasiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'jabatan_lama' => 'required|string|max:255',
+            'jabatan_baru' => 'required|string|max:255',
+            'alasan' => 'required|string',
+            'tanggal_mutasi' => 'required|date',
+        ];
+    }
+}
