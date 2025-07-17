aa2ced42f7980e3f17654e7c02d09a2897089af1
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:27:00 +0700

    Upload resources/views/dashboard/finance.blade.php

---
+++ resources/views/dashboard/finance.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">Dashboard Keuangan</div>
+            <div class="card-body">
+                Selamat datang, Keuangan! Ini adalah dashboard khusus untuk Anda.
+                <!-- Tambahkan konten spesifik Keuangan di sini -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
