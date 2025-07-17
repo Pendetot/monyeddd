d1093d283c63aeb628a734d3366b005d1c4fa2d2
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:28 +0700

    Upload app/Http/Requests/StoreSuratPeringatanRequest.php

---
+++ app/Http/Requests/StoreSuratPeringatanRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreSuratPeringatanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'jenis_sp' => ['required', Rule::in(['SP1', 'SP2', 'SP3'])],
+            'tanggal_sp' => 'required|date',
+            'keterangan' => 'nullable|string',
+        ];
+    }
+}
