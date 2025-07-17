6e5955c98db973a90adfa46a1bc2ea23223c400e
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:04:53 +0700

    Upload app/Http/Requests/Keuangan/Hutang/StoreHutangKaryawanRequest.php

---
+++ app/Http/Requests/Keuangan/Hutang/StoreHutangKaryawanRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Keuangan\Hutang;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreHutangKaryawanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'karyawan_id' => 'required|exists:karyawans,id',
+            'jumlah_hutang' => 'required|numeric|min:0',
+            'tanggal_hutang' => 'required|date',
+            'status' => 'required|in:lunas,belum_lunas',
+        ];
+    }
+}
