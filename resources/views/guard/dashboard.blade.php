b8100d4b4712559d19108db2906ca3562220a932
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:27:04 +0700

    Upload resources/views/guard/dashboard.blade.php

---
+++ resources/views/guard/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Guard Dashboard')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Selamat Datang di Dashboard Guard</h3>
+                </div>
+                <div class="card-body">
+                    <p>Ini adalah dashboard khusus untuk peran Guard.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
