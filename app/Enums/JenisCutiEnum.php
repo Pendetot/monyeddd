892efe164bb2d457ae90898bea1a806c0dd685c1
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:02:23 +0700

    Upload app/Enums/JenisCutiEnum.php

---
+++ app/Enums/JenisCutiEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum JenisCutiEnum: string
+{
+    case Tahunan = 'tahunan';
+    case Sakit = 'sakit';
+    case Melahirkan = 'melahirkan';
+    case Penting = 'penting';
+}
