1f3dd40e3da2be60d8a6b45e528a585c9da1cef4
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:27:03 +0700

    Upload resources/views/dashboard/hrd.blade.php

---
+++ resources/views/dashboard/hrd.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">Dashboard HRD</div>
+            <div class="card-body">
+                Selamat datang, HRD! Ini adalah dashboard khusus untuk Anda.
+                <!-- Tambahkan konten spesifik HRD di sini -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
