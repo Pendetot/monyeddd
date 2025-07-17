b1305fe76a8b4ab097b279465a3d7d6ce86dec72
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:28:44 +0700

    Upload resources/views/operasional/dashboard.blade.php

---
+++ resources/views/operasional/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Operasional Dashboard')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Selamat Datang di Dashboard Operasional</h3>
+                </div>
+                <div class="card-body">
+                    <p>Ini adalah dashboard khusus untuk peran Operasional.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
