8e93cb9eaf294f068afbdbbdc5b7947579f6b4e0
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-18 02:02:55 +0700

    Upload app/Http/Controllers/PembinaanController.php

---
+++ app/Http/Controllers/PembinaanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pembinaan;
+use Illuminate\Http\Request;
+
+class PembinaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pembinaan $pembinaan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pembinaan $pembinaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pembinaan $pembinaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pembinaan $pembinaan)
+    {
+        //
+    }
+}
