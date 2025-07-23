686a8ab73e8e749bd95c0611687e028c458c95b0
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:16:26 -0700

    Update app/Http/Controllers/Keuangan/HutangKaryawanController.php

--- app/Http/Controllers/Keuangan/HutangKaryawanController.php
+++ app/Http/Controllers/Keuangan/HutangKaryawanController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Keuangan;
 
 use App\Http\Controllers\Controller;
+use App\Models\HutangKaryawan;
 use Illuminate\Http\Request;
 
 class HutangKaryawanController extends Controller
 {
     public function index()
     {
-        return view('keuangan.hutang-karyawan');
+        // Eager load the karyawan relationship to avoid N+1 queries
+        $hutangKaryawans = HutangKaryawan::with('user')->latest()->get();
+
+        return view('keuangan.hutang-karyawan', compact('hutangKaryawans'));
     }
 }
