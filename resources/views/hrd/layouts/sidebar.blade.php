e9ff1e8ee6d299246430131d403e2d65305c650e
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:39:54 -0700

    Upload resources/views/hrd/layouts/sidebar.blade.php

---
+++ resources/views/hrd/layouts/sidebar.blade.php
@@ -0,0 +1,47 @@
+<li class="pc-item pc-caption">
+  <label data-i18n="Navigation">Navigation</label>
+  <i class="ph-duotone ph-gauge"></i>
+</li>
+<li class="pc-item">
+  <a href="{{ route('hrd.dashboard') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+  </a>
+</li>
+
+<li class="pc-item pc-caption">
+  <label data-i18n="HRD">HRD</label>
+  <i class="ph-duotone ph-hard-hat"></i>
+</li>
+<li class="pc-item"><a href="{{ route('hrd.data-karyawan') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-users-three"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Data Karyawan">Data Karyawan</span></a></li>
+<li class="pc-item"><a href="{{ route('hrd.pengajuan-cuti') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-calendar-blank"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Pengajuan Cuti">Pengajuan Cuti</span></a></li>
+<li class="pc-item"><a href="{{ route('hrd.data-resign') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-door-open"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Data Resign">Data Resign</span></a></li>
+<li class="pc-item"><a href="{{ route('hrd.surat-peringatan') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-warning-circle"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Surat Peringatan">Surat Peringatan</span></a></li>
+<li class="pc-item"><a href="{{ route('hrd.mutasi-karyawan') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-arrows-left-right"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Mutasi Karyawan">Mutasi Karyawan</span></a></li>
+<li class="pc-item"><a href="{{ route('hrd.administrasi-pelamar.index') }}" class="pc-link">
+    <span class="pc-micon">
+        <i class="ph-duotone ph-user-plus"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Daftar Pelamar">Daftar Pelamar</span></a></li>
