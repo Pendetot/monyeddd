67977335dee2eff21cc23ac2c9aa21469a2ff5eb
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:42:37 -0700

    Update routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Pelamar\PelamarController;
-use App\Http\Controllers\Api\HRD\CutiController;
-use App\Http\Controllers\Api\HRD\ResignController;
-use App\Http\Controllers\Api\HRD\SuratPeringatanController;
-use App\Http\Controllers\Api\HRD\MutasiController;
-use App\Http\Controllers\Api\Keuangan\HutangController;
-use App\Http\Controllers\Api\Keuangan\RekeningController;
-use App\Http\Controllers\Api\Operasional\KPIController;
-use App\Http\Controllers\Api\Operasional\PembinaanController;
-use App\Http\Controllers\Api\Operasional\LapDokumenController;
-use App\Http\Controllers\Api\Operasional\AbsensiController;
 use App\Http\Controllers\Api\Auth\RegisterLoginController;
+use App\Http\Controllers\PelamarController;
+use App\Http\Controllers\OverrideController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,39 +17,36 @@ use App\Http\Controllers\Api\Auth\RegisterLoginController;
 |
 */
 
-use App\Http\Controllers\HRD\AdministrasiPelamarController;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::put('/pelamar/{pelamar}/administrasi', [AdministrasiPelamarController::class, 'update']);
 
-Route::prefix('pelamar')->group(function () {
-    Route::apiResource('/', PelamarController::class);
-});
+
+
 
 Route::prefix('hrd')->group(function () {
-    Route::apiResource('cuti', CutiController::class);
-    Route::apiResource('resign', ResignController::class);
-    Route::apiResource('surat-peringatan', SuratPeringatanController::class);
-    Route::apiResource('mutasi', MutasiController::class);
+    
+    
+    
+    
 });
 
 Route::prefix('keuangan')->group(function () {
-    Route::apiResource('hutang', HutangController::class);
-    Route::apiResource('rekening', RekeningController::class);
+    
+    
 });
 
-Route::prefix('operasional')->group(function () {
-    Route::apiResource('kpi', KPIController::class);
-    Route::apiResource('pembinaan', PembinaanController::class);
-    Route::apiResource('lap-dokumen', LapDokumenController::class);
-    Route::apiResource('absensi', AbsensiController::class);
+Route::prefix('karyawan')->group(function () {
+    
 });
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [RegisterLoginController::class, 'register']);
-    Route::post('login', [RegisterLoginController::class, 'login']);
-    Route::post('logout', [RegisterLoginController::class, 'logout'])->middleware('auth:sanctum');
-});
+Route::get('/pelamar/filter', [PelamarController::class, 'filter']);
+Route::post('/pelamar/{pelamar}/validate', [PelamarController::class, 'performValidation']);
+Route::post('/pelamar/override', [OverrideController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/pelamar/{pelamar}/generate-interview-invitation', [PelamarController::class, 'generateInterviewInvitation']);
+Route::post('/pelamar/{pelamar}/approve', [PelamarController::class, 'approve']);
+
+
