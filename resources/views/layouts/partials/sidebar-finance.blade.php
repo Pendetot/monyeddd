e678f7ffb9b198f44b34f30443d8e369f90e693f
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:41:47 -0700

    Update resources/views/layouts/partials/sidebar-finance.blade.php

--- resources/views/layouts/partials/sidebar-finance.blade.php
+++ resources/views/layouts/partials/sidebar-finance.blade.php
@@ -3,7 +3,7 @@
   <i class="ph-duotone ph-gauge"></i>
 </li>
 <li class="pc-item">
-  <a href="/dashboard" class="pc-link">
+  <a href="{{ route('keuangan.dashboard') }}" class="pc-link">
     <span class="pc-micon">
       <i class="ph-duotone ph-gauge"></i>
     </span>
@@ -13,16 +13,11 @@
 <li class="pc-item pc-caption">
   <label data-i18n="Manajemen Keuangan">Manajemen Keuangan</label>
 </li>
-<li class="pc-item pc-hasmenu">
-  <a href="#!" class="pc-link">
+<li class="pc-item">
+  <a href="{{ route('keuangan.hutang-karyawans.index') }}" class="pc-link">
     <span class="pc-micon">
-      <i class="ph-duotone ph-currency-dollar"></i>
+      <i class="ph-duotone ph-wallet"></i>
     </span>
-    <span class="pc-mtext" data-i18n="Data Keuangan">Data Keuangan</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-  <ul class="pc-submenu">
-    <li class="pc-item"><a class="pc-link" href="{{ route('finance.accounts.index') }}" data-i18n="Data Rekening">Data Rekening</a></li>
-    <li class="pc-item"><a class="pc-link" href="{{ route('finance.salaries.index') }}" data-i18n="Gaji">Gaji</a></li>
-    <li class="pc-item"><a class="pc-link" href="{{ route('finance.debts.index') }}" data-i18n="Hutang">Hutang</a></li>
-    <li class="pc-item"><a class="pc-link" href="{{ route('finance.payment-histories.index') }}" data-i18n="Histori Pembayaran">Histori Pembayaran</a></li>
-  </ul>
+    <span class="pc-mtext" data-i18n="Hutang Karyawan">Hutang Karyawan</span>
+  </a>
 </li>
