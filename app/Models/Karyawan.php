dc9d3a309fdd880a80031b8dd6f81a718e5c0c42
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:17:58 -0700

    Update app/Models/Karyawan.php

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -18,33 +18,4 @@ class Karyawan extends Model
         'penempatan',
         'status',
     ];
-
-    protected $casts = [
-        'status' => \App\Enums\StatusKaryawanEnum::class,
-    ];
-
-    public function cutis()
-    {
-        return $this->hasMany(Cuti::class);
-    }
-
-    public function mutasis()
-    {
-        return $this->hasMany(Mutasi::class);
-    }
-
-    public function resigns()
-    {
-        return $this->hasMany(Resign::class);
-    }
-
-    public function hutangKaryawans()
-    {
-        return $this->hasMany(HutangKaryawan::class);
-    }
-
-    public function kpis()
-    {
-        return $this->hasMany(KPI::class);
-    }
 }
