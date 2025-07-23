74efb36aafcd1f22cae0e69423548dd60e00439f
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:40:14 -0700

    Upload resources/views/hrd/resigns/resign/index.blade.php

---
+++ resources/views/hrd/resigns/resign/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('title', 'Manajemen Resign')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 col-xl-12">
+            <div class="card table-card">
+                <div class="card-header d-flex align-items-center justify-content-between py-3">
+                    <h5>Daftar Karyawan Resign</h5>
+                    <div class="dropdown">
+                        <a class="avtar avtar-xs btn-link-secondary dropdown-toggle arrow-none" href="#"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                                class="material-icons-two-tone f-18">more_vert</i></a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="#">Tambah Data Resign Baru</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body py-2 px-0">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    <div class="table-responsive">
+                        <table class="table table-hover table-borderless table-sm mb-0">
+                            <tbody>
+                                {{-- Data akan ditampilkan di sini --}}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
