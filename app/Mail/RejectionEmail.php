d06f7258305b4f99f3f214222a1a93d7fdafa4ed
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:17:46 -0700

    Upload app/Mail/RejectionEmail.php

---
+++ app/Mail/RejectionEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class RejectionEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Pemberitahuan Hasil Seleksi Lamaran Kerja',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.rejection',
+            with: [
+                'nama_pelamar' => $this->data['nama_pelamar'],
+                'greeting' => $this->data['greeting'],
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
