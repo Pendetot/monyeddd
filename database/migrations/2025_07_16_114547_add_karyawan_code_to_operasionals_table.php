c4c9b0cc0eea7e7b55a87f73e0b28969371786e8
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:19:32 -0700

    Update database/migrations/2025_07_16_114547_add_karyawan_code_to_operasionals_table.php

--- database/migrations/2025_07_16_114547_add_karyawan_code_to_operasionals_table.php
+++ database/migrations/2025_07_16_114547_add_karyawan_code_to_operasionals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('operasionals', function (Blueprint $table) {
+        Schema::table('karyawan_operasionals', function (Blueprint $table) {
             $table->string('karyawan_code')->unique()->nullable()->after('id');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('operasionals', function (Blueprint $table) {
+        Schema::table('karyawan_operasionals', function (Blueprint $table) {
             $table->dropColumn('karyawan_code');
         });
     }
