8915f5008bb96c9b6e11fb6d3fa7567387d96769
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:41:49 -0700

    Upload resources/views/logistik/layouts/sidebar.blade.php

---
+++ resources/views/logistik/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<li class="pc-item pc-caption">
+  <label data-i18n="Navigasi">Navigasi</label>
+  <i class="ph-duotone ph-gauge"></i>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.dashboard') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+  </a>
+</li>
+<li class="pc-item pc-caption">
+  <label data-i18n="Manajemen Logistik">Manajemen Logistik</label>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.absensis.index') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-clock"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Absensi">Absensi</span>
+  </a>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.lap-dokumens.index') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-file-text"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Laporan Dokumen">Laporan Dokumen</span>
+  </a>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.pembinaans.index') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-users"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Pembinaan">Pembinaan</span>
+  </a>
+</li>
