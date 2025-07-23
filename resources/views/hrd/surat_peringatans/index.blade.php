f909e8ecdf3cd7d296073a3f59aa127b6851929c
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:40:18 -0700

    Update resources/views/hrd/surat_peringatans/index.blade.php

--- resources/views/hrd/surat_peringatans/index.blade.php
+++ resources/views/hrd/surat_peringatans/index.blade.php
@@ -4,56 +4,71 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Daftar Surat Peringatan</h3>
-                    <div class="card-tools">
-                        <a href="{{ route('surat-peringatans.create') }}" class="btn btn-primary btn-sm">Tambah Surat Peringatan</a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    @if (session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Karyawan</th>
-                                <th>Jenis SP</th>
-                                <th>Tanggal SP</th>
-                                <th>Keterangan</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($suratPeringatans as $sp)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $sp->karyawan->nama }}</td>
-                                    <td>{{ $sp->jenis_sp->value }}</td>
-                                    <td>{{ $sp->tanggal_sp->format('d-m-Y') }}</td>
-                                    <td>{{ $sp->keterangan }}</td>
-                                    <td>
-                                        <a href="{{ route('surat-peringatans.edit', $sp->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                        <form action="{{ route('surat-peringatans.destroy', $sp->id) }}" method="POST" style="display: inline-block;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+  <div class="row">
+    <div class="col-md-12 col-xl-12">
+      <div class="card table-card">
+        <div class="card-header d-flex align-items-center justify-content-between py-3">
+          <h5>Daftar Surat Peringatan</h5>
+          <div class="dropdown">
+            <a class="avtar avtar-xs btn-link-secondary dropdown-toggle arrow-none" href="#"
+               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="material-icons-two-tone f-18">more_vert</i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end">
+              <a class="dropdown-item" href="{{ route('hrd.surat-peringatan.create') }}">Tambah SP</a>
             </div>
+          </div>
         </div>
+        <div class="card-body py-2 px-0">
+          <div class="table-responsive">
+            <table class="table table-hover table-borderless table-sm mb-0">
+              <tbody>
+                @foreach($suratPeringatans as $sp)
+                  <tr>
+                    <td>
+                      <div class="d-inline-block align-middle">
+                        <div class="d-inline-block">
+                          <h6 class="m-b-0">{{ $sp->karyawan->nama }}</h6>
+                          <p class="m-b-0">{{ $sp->tanggal_sp->format('d M Y') }}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p class="m-b-0">{{ ucfirst($sp->jenis_sp->value) }}</p>
+                    </td>
+                    <td class="text-end">
+                      <div class="d-inline-flex">
+                        
+                        
+                        <form action="{{ route('hrd.surat-peringatan.destroy', $sp->id) }}"
+                              method="POST" class="d-inline">
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit"
+                                  class="btn avtar avtar-xs btn-light-danger"
+                                  onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
+                            <i class="ti ti-trash"></i>
+                          </button>
+                        </form>
+                      </div>
+                    </td>
+                  </tr>
+                @endforeach
+                @unless($suratPeringatans->count())
+                  <tr>
+                    <td colspan="3" class="text-center py-4 text-muted">
+                      Belum ada surat peringatan.
+                    </td>
+                  </tr>
+                @endunless
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
+
+@include('components.delete-confirmation-modal')
 @endsection
