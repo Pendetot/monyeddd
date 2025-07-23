59d5989d87cec6d80ad5dfe030a1481c9ccd2b4b
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-07-23 10:19:25 -0700

    Update database/migrations/2025_07_15_192944_create_operasionals_table.php

--- database/migrations/2025_07_15_192944_create_operasionals_table.php
+++ database/migrations/2025_07_15_192944_create_operasionals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('operasionals', function (Blueprint $table) {
+        Schema::create('karyawan_operasionals', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('email')->unique();
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('operasionals');
+        Schema::dropIfExists('karyawan_operasionals');
     }
 };
